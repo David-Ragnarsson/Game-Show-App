a323ce4832379e239a0ba72476aa472f1589da6a
David-Ragnarsson <108486409+David-Ragnarsson@users.noreply.github.com>
2023-03-03 17:32:46 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,16 @@
 
-
-
-const qwerty = documentQuerySelector("#qwerty");
+const qwerty = document.querySelector("#qwerty");
 
 const phrase = documentQuerySelector("#phrase");
 
-const btn__reset = documentQuerySelector("#btn__reset");
+const btn__reset = documentQuerySelector(".btn__reset");
 
 const overlay = documentQuerySelector("#overlay");
 
 var missed = 
 
-btn__reset.addEventListener('click') {
-    overlay.display = 'none';
+btn__reset.addEventListener('click', () => {
+    overlay.style.display = 'none';
 });
 
 
@@ -23,4 +21,40 @@ phrases = [
     'I Love Chocolate',
     'Timing is everything',
     'Coffee Beans'
-  ],
+  ]
+
+  // return a random phrase from an array
+  const getRandomPhraseAsArray = arr => {
+
+
+  }
+
+  // adds the letters of a string to the display
+  const addPhraseToDisplay = arr => {
+
+
+  }
+
+  // check if a letter is in the phrase
+  const checkLetter = button => {
+
+
+  }
+
+  //check if the game has been won or lost
+  const checkWin = {} => {
+
+
+  }
+
+  // listern for the start game button to be pressed
+  startButton.addEventListener('click', () =>) {
+
+
+  }
+
+  // listen for the onscreen keyboard to be clicked
+  qwerty.addEventListener('click', e => {
+
+
+  });
