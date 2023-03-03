811da8c21ee1d1a3864b1d4efe21cdd0dd653928
David-Ragnarsson <108486409+David-Ragnarsson@users.noreply.github.com>
2023-03-03 12:42:58 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,26 @@
-baguetteBox.run('.gallery');
-const search = new Filter('search', 'data-caption');
+
+
+
+const qwerty = documentQuerySelector("#qwerty");
+
+const phrase = documentQuerySelector("#phrase");
+
+const btn__reset = documentQuerySelector("#btn__reset");
+
+const overlay = documentQuerySelector("#overlay");
+
+var missed = 
+
+btn__reset.addEventListener('click') {
+    overlay.display = 'none';
+});
+
+
+
+phrases = [
+    'I Love Pizza',
+    'JavaScript is Cool',
+    'I Love Chocolate',
+    'Timing is everything',
+    'Coffee Beans'
+  ],
